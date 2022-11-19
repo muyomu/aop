4836c3b64889a2359c931d139da599cbd71f0045
mutian <bhfqlz@qq.com>
2022-11-19 08:26:00 +0800

    notices:
    version 1.0.6

--- src/FrameWorkClient.php
+++ src/FrameWorkClient.php
@@ -4,11 +4,7 @@ namespace muyomu\aop;
 
 use Exception;
 use muyomu\aop\advice\FrameWork;
-use muyomu\aop\advicetype\AfterAdvice;
-use muyomu\aop\advicetype\BeforeAdvice;
-use muyomu\aop\advicetype\BeforeCatchAdvice;
 use muyomu\aop\advicetype\Hystrix;
-use muyomu\aop\advicetype\ReturnedAdvice;
 use ReflectionClass;
 use ReflectionException;
 use ReflectionMethod;
@@ -21,60 +17,21 @@ class FrameWorkClient implements FrameWork
      */
     public function aopExecutor(mixed $instance, ReflectionMethod $method, mixed $args): mixed
     {
-        $beforeAdvice = $method->getAttributes(BeforeAdvice::class);
-        $afterAdvice = $method->getAttributes(AfterAdvice::class);
-        $returnedAdvice = $method->getAttributes(ReturnedAdvice::class);
-        $beforeCatchAdvice = $method->getAttributes(BeforeCatchAdvice::class);
         $Hystrix = $method->getAttributes(Hystrix::class);
-
-        if (!empty($beforeAdvice)){
-            $advice = $beforeAdvice[0]->newInstance();
-            $adviceClass = $advice->getAdviceClassName();
-            $adviceClass_class = new ReflectionClass($adviceClass);
-            $adviceClass_instance = $adviceClass_class->newInstance();
-            $adviceClass_handle = $adviceClass_class->getMethod("adviceHandle");
-            $adviceClass_handle->invoke($adviceClass_instance,$args);
-        }
 
         try {
             $data = $method->invokeArgs($instance,$args);
         }catch (Exception $exception){
-            if (!empty($beforeCatchAdvice)){
-                $advice = $beforeCatchAdvice[0]->newInstance();
-                $adviceClass = $advice->getAdviceClassName();
-                $adviceClass_class = new ReflectionClass($adviceClass);
-                $adviceClass_instance = $adviceClass_class->newInstance();
-                $adviceClass_handle = $adviceClass_class->getMethod("adviceHandle");
-                $adviceClass_handle->invoke($adviceClass_instance,$args);
-            }
             if(!empty($Hystrix)){
                 $advice = $Hystrix[0]->newInstance();
                 $adviceClass = $advice->getHystrixClassName();
                 $adviceClass_class = new ReflectionClass($adviceClass);
                 $adviceClass_instance = $adviceClass_class->newInstance();
                 $adviceClass_handle = $adviceClass_class->getMethod("getData");
-                return $adviceClass_handle->invoke($adviceClass_instance,$args);
+                return $adviceClass_handle->invoke($adviceClass_instance);
             }else{
                 throw $exception;
             }
-        }
-
-        if (!empty($afterAdvice)){
-            $advice = $afterAdvice[0]->newInstance();
-            $adviceClass = $advice->getAdviceClassName();
-            $adviceClass_class = new ReflectionClass($adviceClass);
-            $adviceClass_instance = $adviceClass_class->newInstance();
-            $adviceClass_handle = $adviceClass_class->getMethod("adviceHandle");
-            $adviceClass_handle->invoke($adviceClass_instance,$args);
-        }
-
-        if (!empty($returnedAdvice)){
-            $advice = $returnedAdvice[0]->newInstance();
-            $adviceClass = $advice->getAdviceClassName();
-            $adviceClass_class = new ReflectionClass($adviceClass);
-            $adviceClass_instance = $adviceClass_class->newInstance();
-            $adviceClass_handle = $adviceClass_class->getMethod("adviceHandle");
-            $adviceClass_handle->invoke($adviceClass_instance,$args);
         }
         return $data;
     }
